a866565976800d0bf25e80382209768f4e4922ed
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-23 19:03:44 -0300

    Creando Tabla para mostrar clientes

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Cliente from "../components/Cliente";
 
 const Inicio = () => {
   const [clientes, setClientes] = useState([]);
@@ -20,7 +21,25 @@ const Inicio = () => {
 
   return (
     <div>
-      <h1>Inicio</h1>
+      <>
+        <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+        <p className="mt-3">Administra tus Clientes</p>
+        <table className="w-full mt-5 table-auto shadow bg-white">
+          <thead className="bg-blue-800 text-white">
+            <tr>
+              <th className="p-2">Nombre</th>
+              <th className="p-2">Contacto</th>
+              <th className="p-2">Empresa</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clientes.map((cliente) => (
+              <Cliente key={cliente.id} cliente={cliente} />
+            ))}
+          </tbody>
+        </table>
+      </>
     </div>
   );
 };
