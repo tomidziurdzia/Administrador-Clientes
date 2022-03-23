dc256abfa53c7510654f6da6f007e17d824f5427
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-23 14:11:13 -0300

    Creando formulario con Formik

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Formik, Form, Field } from "formik";
 
 const Formulario = () => {
   return (
@@ -6,6 +7,71 @@ const Formulario = () => {
       <h1 className="text-gray-600 font-bold text-xl text-center">
         Agregar Cliente
       </h1>
+      <Formik>
+        <Form className="mt-10 ">
+          <div className="mb-4">
+            <label className="text-gray-800" htmlfor="nombre">
+              Nombre:
+            </label>
+            <Field
+              id="nombre"
+              type="text"
+              className="mt-2 block w-full p-3 bg-gray-50"
+              placeholder="Nombre del Cliente"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="text-gray-800" htmlfor="empresa">
+              Empresa:
+            </label>
+            <Field
+              id="empresa"
+              type="text"
+              className="mt-2 block w-full p-3 bg-gray-50"
+              placeholder="Empresa del Cliente"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="text-gray-800" htmlfor="email">
+              Email:
+            </label>
+            <Field
+              id="email"
+              type="email"
+              className="mt-2 block w-full p-3 bg-gray-50"
+              placeholder="Email del Cliente"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="text-gray-800" htmlfor="telefono">
+              Telefono:
+            </label>
+            <Field
+              id="telefono"
+              type="tel"
+              className="mt-2 block w-full p-3 bg-gray-50"
+              placeholder="Telefono del Cliente"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="text-gray-800" htmlfor="notas">
+              Notas:
+            </label>
+            <Field
+              as="textarea"
+              id="notas"
+              type="text"
+              className="mt-2 block w-full p-3 bg-gray-50 h-40"
+              placeholder="Notas del Cliente"
+            />
+          </div>
+          <input
+            type="submit"
+            value="Agregar Cliente"
+            className="mt-5 w-full bg-blue-800 p-3 text-white text-lg font-bold rounded-md"
+          />
+        </Form>
+      </Formik>
     </div>
   );
 };
