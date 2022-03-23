b40012f58b741288f732ab952ad29c6af30dfb27
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-23 13:39:51 -0300

    Creando las paginas para el proyecto

--- src/paginas/NuevoCliente.jsx
+++ src/paginas/NuevoCliente.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import Formulario from "../components/Formulario";
 
 const NuevoCliente = () => {
   return (
-    <div>
-      <h1>NuevoCliente</h1>
-    </div>
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
+      <p className="mt-3">
+        Llena los siguientes campos para registrar un cliente
+      </p>
+      <Formulario />
+    </>
   );
 };
 
