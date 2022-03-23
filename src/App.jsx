cecb360c98931e9acdea37eb26bda9b245e3d686
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-23 11:37:52 -0300

    Configurando Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-
-  return (
-    <h1>CRM React</h1>
-  )
+  return <h1>CRM React</h1>;
 }
 
-export default App
+export default App;
