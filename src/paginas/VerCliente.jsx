441ff0bc61da415936b57a0608260e187f05281e
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-24 16:55:36 -0300

    Mostrando la info de un cliente por su ID

--- src/paginas/VerCliente.jsx
+++ src/paginas/VerCliente.jsx
@@ -1,10 +1,55 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const VerCliente = () => {
+  const [cliente, setCliente] = useState({});
+
   const { id } = useParams();
+
+  useEffect(() => {
+    const obtenerClienteAPI = async () => {
+      try {
+        const url = `http://localhost:4000/clientes/${id}`;
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        setCliente(resultado);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    obtenerClienteAPI();
+  }, []);
+
+  const { nombre, empresa, email, telefono, notas } = cliente;
+
   return (
     <div>
-      <h1>VerCliente</h1>
+      <h1 className="font-black text-4xl text-blue-900">Ver Cliente</h1>
+      <p className="mt-3">Informacion del Cliente</p>
+      <p className="text-4xl text-gray-600 mt-10">
+        <span className="text-gray-800 font-bold">Cliente: </span>
+        {nombre}
+      </p>
+      <p className="text-2xl text-gray-600 mt-4">
+        <span className="text-gray-800 font-bold">Empresa: </span>
+        {empresa}
+      </p>
+      <p className="text-2xl text-gray-600 mt-4">
+        <span className="text-gray-800 font-bold">Email: </span>
+        {email}
+      </p>
+      {telefono && (
+        <p className="text-2xl text-gray-600 mt-4">
+          <span className="text-gray-800 font-bold">Telefono: </span>
+          {telefono}
+        </p>
+      )}
+      {notas && (
+        <p className="text-2xl text-gray-600 mt-4">
+          <span className="text-gray-800 font-bold">Notas: </span>
+          {notas}
+        </p>
+      )}
     </div>
   );
 };
