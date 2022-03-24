8b31a08c3b5f6b4bad3562f0b073674e9adf8201
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-03-24 19:46:34 -0300

    Parte 5 - Mostrando un mensaje de no valido cuando el cliente no existe

--- src/paginas/EditarCliente.jsx
+++ src/paginas/EditarCliente.jsx
@@ -21,13 +21,18 @@ const EditarCliente = () => {
     };
     obtenerClienteAPI();
   }, []);
+
   return (
     <div>
       <h1 className="font-black text-4xl text-blue-900">Editar Cliente</h1>
       <p className="mt-3">
         Utiliza este formulario para editar datos de un cliente
       </p>
-      <Formulario cliente={cliente} cargando={cargando} />
+      {cliente?.nombre ? (
+        <Formulario cliente={cliente} cargando={cargando} />
+      ) : (
+        <p>Cliente ID no valido</p>
+      )}
     </div>
   );
 };
